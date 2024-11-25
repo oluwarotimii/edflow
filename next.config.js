3b78a6eab179d1b17d8692b4fd5adceecc744497
oluwarotimii <oluwarotimiadewumi@gmail.com>
2024-11-25 14:59:48 +0100

    config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true, // optional, enables SWC minification
   // You can add more config options based on your needs here
 };
-
-export default nextConfig;
